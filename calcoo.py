8d37065e292b167f4df0abc4ac85ccae0ef04fea
diegokemp <da.sanchezm@alumnos.urjc.es>
2015-09-30 19:09:51 +0200

    suma y resta

--- calcoo.py
+++ calcoo.py
@@ -5,16 +5,27 @@ import sys
 
 class Calculadora():
 
-    def __init__(self, operando1, operacion, operando2):
+    def __init__(self, operando1, operando2):
         self.operando1 = operando1
-        self.operacion = operacion
         self.operando2 = operando2
 
     def suma(self):
         return self.operando1 + self.operando2
+
+    def resta(self):
+        return self.operando1 - self.operando2
     
 
 if __name__ == "__main__":
+    try:
+        variable = Calculadora(float(sys.argv[1]), float(sys.argv[3]))
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
 
-        variable = Calculadora(float(sys.argv[1]), sys.argv[2], float(sys.argv[3]))
+    if sys.argv[2] == "suma":
         print(variable.suma())
+    elif sys.argv[2] == "resta":
+        print(variable.resta())
+    else:
+        sys.exit('Este programa solo admite "suma" o "resta" como segundo argumento')
+
